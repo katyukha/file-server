93ddbe709fdd6d14a874b59b8f0575051454981d
Dmytro Katyukha <firemage.dima@gmail.com>
2012-10-13 22:39:25 +0300

    Simply works.

--- apps/uploader/controllers.py
+++ apps/uploader/controllers.py
@@ -6,47 +6,61 @@ from bottle import mako_template as template
 from bottle import request
 from bottle import static_file
 from bottle import HTTPError
-from bottle.ext import sqlalchemy
 
 from application import uploader
 
-from models import File
 
 from settings import MEDIA_ROOT
 
-@uploader.get('/', name='home')
-@view('index.html')
-def index(db):
-    files = db.query(File).all()
+from utils import save_file
+from utils import AttrDict
+
+@uploader.get('/', name='home',apply=[view('uploader/index.mako')])
+def index(rdb):
+    file_ids = rdb.lrange('file:global:ids',-11,-1) or []    # get ids of last files
+    file_ids.reverse()
+    files    = []
+    for file_id in file_ids:
+        obj = rdb.hgetall('file:%s:data' % file_id)
+        obj = AttrDict(obj)
+        files.append(obj)
+
     return {
              'files' : files,
              'url'   : uploader.get_url,
            }
 
-@uploader.post('/upload/', name='upload')
-def upload(db):
+@uploader.post('/upload/', name='upload', apply=[view('uploader/upload.mako')])
+def upload(rdb):
     name = request.forms.name
-    data = request.files.data
-    comment = request.forms.comment
-    if name and data!=u'' and data.file:
-        raw = data.file.read()
-        filename = data.filename
-        f = open(MEDIA_ROOT+filename, 'wb')
-        f.write(raw)
-        f.close()
-        file_obj = File(name, filename, comment)
-        db.add(file_obj)
-        return { 'file' : file,
+    filedata = request.files.file
+    description = request.forms.description
+    if name and filedata!=u'' and filedata.file:
+        id = rdb.incr('file:global:last-id')
+        filename = filedata.filename
+
+        filelen = save_file(filedata.file, "%s.%s" % (id,filename))
+
+        #writing filedata to redis
+        data = dict( id = id,
+                     name = name,
+                     filename = filename,
+                   )
+        if description:
+            data.update(description=description)
+
+        rdb.hmset('file:%s:data'%id, data)
+        rdb.rpush('file:global:ids', id)
+        return { 'filename'    : filename,
+                 'filelen'     : filelen,
                  'file_loaded' : True }
 
     return {'file_loaded' : False}
 
-@uploader.get('/file/<filename>', name='file')
-def send_file(filename, db):
-    file = db.query(File).filter_by(filename=filename).first()
-    if file:
-        return static_file(file.filename, root = MEDIA_ROOT)
-    return HTTPError(404, 'File <%s(%s)> not found.' % (file.name, file.filename))
+@uploader.get('/file/<id>/<filename>', name='file')
+def send_file(id, filename, rdb):
+    return static_file("%s.%s" % (id,filename), root = MEDIA_ROOT)
+    return HTTPError(404, 'File not found.')
 
 
 
--- apps/uploader/models.py
+++ apps/uploader/models.py
@@ -1,20 +1,24 @@
-from sqlalchemy import Column, Integer, Sequence, String, Text
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+class File():
+
+    fields = [ 'id',
+               'name',
+               'filename',
+               'description'
+             ]
+
+    def __init__(self, rdb):
+        """ param rdb: instance of redis.Redis class
+        """
+        self.rdb = rdb
+
+    def init(self):
+        """perform some initialization
+        """
+
+    def create(self):
 
 
-class File(Base):
-    __tablename__ = 'downloader_file'
-    id          = Column(Integer, Sequence('id_seq'), primary_key=True)
-    name        = Column(String(256))
-    filename    = Column(String(256))
-    comment     = Column(Text())
-
-    def __init__(self, name, filename, comment=None):
-        self.name     = name
-        self.filename = filename
-        self.comment  = comment
 
     def __repr__(self):
         return "<Entity('%d', '%s')>" % (self.id, self.name)
